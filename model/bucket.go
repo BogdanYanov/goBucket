405c928a88a8f19346f3e52e8aa1d6784f90bf18
Bogdan Yanov <b.yanov76@gmail.com>
2019-12-02 13:39:17 +0200

    Create the second searching function

--- model/bucket.go
+++ model/bucket.go
@@ -42,3 +42,34 @@ func (b *Bucket) Order(order ...int) {
 		}
 	}
 }
+
+// Order2 search order items in bucket vertically using labels instead 'for'
+func (b *Bucket) Order2(order ...int) {
+	var (
+		curList int
+		curItem int
+	)
+
+Order:
+	for _, val := range order {
+		curList = 0
+		curItem = 0
+
+	Search:
+		if val == (*b)[curList][curItem] {
+			(*b)[curList][curItem] = 0
+			continue Order
+		} else {
+			if curList < len(*b)-1 {
+				curList++
+				goto Search
+			} else if curItem < len((*b)[0])-1 {
+				curList = 0
+				curItem++
+				goto Search
+			} else {
+				continue Order
+			}
+		}
+	}
+}
